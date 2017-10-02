edf51569473bdce5a0f5be9372587c0ff889405e
gozingdown <gongzhenggz@gmail.com>
2017-10-02 00:01:35 -0400

    rest services, promises, use json-server and http-server to develop

--- Services/ContactApp/contact.controller.js
+++ Services/ContactApp/contact.controller.js
@@ -3,12 +3,37 @@
     app.controller("ContactCtrl", ContactCtrl)
 
     function ContactCtrl(ContactDataSvc) {
-        this.contacts = ContactDataSvc.contacts
+        this.contacts = []
+        this.isEdit = false
+
+        var self = this
+        ContactDataSvc.getContacts()
+            .then(function(data){
+                self.contacts = data
+                console.log(self.contacts)
+            })
 
         this.selectedContact = this.contacts[0]
 
         this.selectContact = function(index) {
             this.selectedContact = this.contacts[index]
+            this.successMessage = undefined
+            self.errorMessage = undefined
+        }
+
+        this.toggleEditMode = function() {
+            this.isEdit = !this.isEdit;
+        }
+
+        this.saveUser = function() {
+            var self = this
+            self.toggleEditMode()
+            var userData = self.selectedContact
+            ContactDataSvc.saveUser(userData).then(function() {
+                self.successMessage = "Data successfully updated" // use `self` otherwise the function becomes a constructor
+            }, function() {
+                self.errorMessage = "There was an error. Please try again"
+            }) // you can have the error handler anywhere in the promise train
         }
     }
 })();
